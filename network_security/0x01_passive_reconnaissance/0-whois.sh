12a8aa269c20a8792f5b9cf3476e58d879f40d9f
respect318 <ferid.mirzezade.education@gmail.com>
2025-08-17 17:28:29 +0400

    0-whois.sh

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk '/^(Registrant|Admin|Tech)/ {gsub(/^[ \t]+|[ \t]+$/, "", $0); split($0,a,":"); gsub(/^ /,"",a[2]); print a[1]","a[2]}' > $1.csv
+whois $1 | awk -F: '/Registrant|Admin|Tech/ { gsub(/^ +| +$/,"",$2); print $1","$2 }'
