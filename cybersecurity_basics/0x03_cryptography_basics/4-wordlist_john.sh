a83f0314d8a8ae5eb828fd469d50d0f4e756b37c
respect318 <ferid.mirzezade.education@gmail.com>
2025-10-24 20:08:36 +0400

    4-wordlist_john.sh

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --show "$HASHFILE" | awk -F: '/:/{print $2}' | sed '/^$/d' > 4-password.txt
+[[ -z "${1:-}" ]] && { echo "Usage: $0 <hash_file>"; exit 1; }; [[ -f /usr/share/wordlists/rockyou.txt ]] && WL=/usr/share/wordlists/rockyou.txt || WL=<(zcat /usr/share/wordlists/rockyou.txt.gz 2>/dev/null) || WL=wordlist.txt; john --wordlist="$WL" "$1" && john --show "$1" | awk -F: '/:/{print $2}' | sed '/^$/d' > 4-password.txt
