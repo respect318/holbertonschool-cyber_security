0281ca5261accb63c5b6a9f2686feb22fdacf5a0
respect318 <ferid.mirzezade.education@gmail.com>
2025-10-25 06:59:22 -0400

    5-windows_john.sh

--- cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/ --format=NT "$1"| cut -d ' ' -f 2 >5-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt --format=NT "$1"| cut -d ' ' -f 2 >5-password.txt
