a2de2fc86e429c521a3c7f4773312abdcba5cd17
respect318 <ferid.mirzezade.education@gmail.com>
2025-12-02 07:17:58 -0500

    ...

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tail -n 1000 auth.log | grep "Accepted password" | awk '{print $9}' | sort | uniq
+tail -n 1000 auth.log | grep "Failed password" | awk '{print $9}' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
