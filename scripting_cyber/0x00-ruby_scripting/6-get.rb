7854f36ac56d6b2256182af195d28a12be29958e
respect318 <ferid.mirzezade.education@gmail.com>
2026-01-17 00:43:01 +0400

    ruby_language

--- scripting_cyber/0x00-ruby_scripting/6-get.rb
+++ scripting_cyber/0x00-ruby_scripting/6-get.rb
@@ -3,17 +3,19 @@ require 'uri'
 require 'json'
 
 def get_request(url)
-  uri = URI.parse(url)
+  uri = URI(url)
   response = Net::HTTP.get_response(uri)
 
-  puts "Response status: #{response.code} #{response.message}"
-  puts "\nResponse body:\n"
+  # Status
+  print "Response status: #{response.code} #{response.message}\n"
+
+  # Body
+  print "\nResponse body:\n\n"
 
   begin
     json_body = JSON.parse(response.body)
-    puts JSON.pretty_generate(json_body)
+    print JSON.pretty_generate(json_body)
   rescue JSON::ParserError
-    # Əgər JSON deyil, sadəcə çap et
-    puts response.body
+    print response.body
   end
 end
