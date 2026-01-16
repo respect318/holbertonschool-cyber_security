cd4a8d9c8f4fe5adad8bc6e760efa59bf1bbbd9c
respect318 <ferid.mirzezade.education@gmail.com>
2026-01-17 00:04:28 +0400

    ruby_language

--- scripting_cyber/0x00-ruby_scripting/3-read_file.rb
+++ scripting_cyber/0x00-ruby_scripting/3-read_file.rb
@@ -1,22 +1,14 @@
 require 'json'
 
 def count_user_ids(path)
-  # 1. Faylı oxu
-  file_content = File.read(path)
+  data = JSON.parse(File.read(path))
 
-  # 2. JSON-u Ruby obyektinə çevir
-  data = JSON.parse(file_content)
-
-  # 3. userId-ləri saymaq üçün hash
   counts = Hash.new(0)
 
-  # 4. Hər element üzrə userId say
   data.each do |item|
-    user_id = item["userId"]
-    counts[user_id] += 1
+    counts[item["userId"]] += 1
   end
 
-  # 5. Nəticəni çap et
   counts.each do |user_id, count|
     puts "#{user_id}: #{count}"
   end
--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -1,15 +1,12 @@
 require 'json'
 
 def merge_json_files(file1_path, file2_path)
-  # file1 -> əlavə ediləcək
-  # file2 -> əsas fayl
+  json1 = JSON.parse(File.read(file1_path))
+  json2 = JSON.parse(File.read(file2_path))
 
-  data1 = JSON.parse(File.read(file1_path))
-  data2 = JSON.parse(File.read(file2_path))
+  merged = json2 + json1
 
-  merged = data2 + data1
-
-  File.open(file2_path, 'w') do |file|
-    file.write(JSON.pretty_generate(merged))
+  File.open(file2_path, 'w') do |f|
+    f.write(JSON.pretty_generate(merged))
   end
 end
