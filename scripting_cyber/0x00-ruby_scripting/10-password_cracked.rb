990490c070f363541755717630f674af5313d3f8
respect318 <ferid.mirzezade.education@gmail.com>
2026-01-17 01:03:43 +0400

    ruby_language

---
+++ scripting_cyber/0x00-ruby_scripting/10-password_cracked.rb
@@ -0,0 +1,26 @@
+require 'digest'
+
+# Check arguments
+if ARGV.length != 2
+  puts "Usage: 10-passwordcracked.rb HASHEDPASSWORD DICTIONARY_FILE"
+  exit
+end
+
+hashed_password = ARGV[0]
+dictionary_file = ARGV[1]
+
+found = false
+
+File.foreach(dictionary_file) do |word|
+  word = word.strip
+  hash = Digest::SHA256.hexdigest(word)
+
+  if hash == hashed_password
+    puts "Password found: #{word}"
+    found = true
+    break
+  end
+end
+
+puts "Password not found in dictionary." unless found
+
