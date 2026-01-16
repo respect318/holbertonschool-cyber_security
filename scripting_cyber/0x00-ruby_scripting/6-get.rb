67e58f2e464e0e845f0673ab2f9d5a508afc500b
respect318 <ferid.mirzezade.education@gmail.com>
2026-01-17 00:44:12 +0400

    ruby_language

--- scripting_cyber/0x00-ruby_scripting/6-get.rb
+++ scripting_cyber/0x00-ruby_scripting/6-get.rb
@@ -7,13 +7,14 @@ def get_request(url)
   response = Net::HTTP.get_response(uri)
 
   # Status
-  print "Response status: #{response.code} #{response.message}\n"
+  print "Response status: #{response.code} #{response.message}\n\n"
 
   # Body
-  print "\nResponse body:\n\n"
+  print "Response body:\n\n"
 
   begin
     json_body = JSON.parse(response.body)
+    # JSON pretty_generate, sonda extra newline qoyma
     print JSON.pretty_generate(json_body)
   rescue JSON::ParserError
     print response.body
