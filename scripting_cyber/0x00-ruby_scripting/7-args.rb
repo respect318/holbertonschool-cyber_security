6ed86b4e6109a782f7f65d782d88ffab7e5138bb
respect318 <ferid.mirzezade.education@gmail.com>
2026-01-17 00:55:27 +0400

    ruby_language

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -1,9 +1,10 @@
 def print_arguments
   if ARGV.empty?
-    puts "No arguments provided."
+      puts "No arguments provided."
   else
-    ARGV.each do |arg|
-      puts arg
-    end
+      puts "Arguments:"
+      ARGV.each_with_index do |arg, index|
+          puts "#{index +1}. #{arg}"
+      end
   end
 end
