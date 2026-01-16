f370b0a0d2e054d804d05c81e6cc1e73e7491652
respect318 <ferid.mirzezade.education@gmail.com>
2026-01-17 01:01:47 +0400

    ruby_language

--- scripting_cyber/0x00-ruby_scripting/9-download_file.rb
+++ scripting_cyber/0x00-ruby_scripting/9-download_file.rb
@@ -1,26 +1,27 @@
 require 'open-uri'
+require 'uri'
 require 'fileutils'
 
-# Check for arguments
+def download_file(url, local_path)
+    if url.nil? || local_path.nil?
+        puts "Usage: 9-download_file.rb URL LOCAL_FILE_PATH"
+        return
+    end
+
+    FileUtils.mkdir_p(File.dirname(local_path))
+    puts "Downloading file from #{url}..."
+    URI.open(url) do |file|
+        File.open(local_path, 'wb') do |out_file|
+            out_file.write(file.read)
+        end
+    end
+    puts "File downloaded and saved to #{local_path}"
+end
+
 if ARGV.length != 2
-  puts "Usage: 9-download_file.rb URL LOCALFILE_PATH"
-  exit
+    puts "Usage: 9-download_file.rb URL LOCAL_FILE_PATH"
+else
+    url = ARGV[0]
+    local_path = ARGV[1]
+    download_file(url, local_path)
 end
-
-url = ARGV[0]
-local_path = ARGV[1]
-
-puts "Downloading file from #{url}…"
-
-# Create folder if not exists
-dir = File.dirname(local_path)
-FileUtils.mkdir_p(dir) unless Dir.exist?(dir)
-
-# Download and save
-URI.open(url) do |remote_file|
-  File.open(local_path, "wb") do |file|
-    file.write(remote_file.read)
-  end
-end
-
-puts "File downloaded and saved to #{local_path}."
