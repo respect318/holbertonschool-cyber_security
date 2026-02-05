fb28b9821f2e60822fd23b74c5b9c4099d6bcd11
respect318 <ferid.mirzezade.education@gmail.com>
2026-02-05 09:53:57 +0400

    task

--- python_for_cyber/4x00_python_security/0x00_python_security/breach_check.py
+++ python_for_cyber/4x00_python_security/0x00_python_security/breach_check.py
@@ -11,7 +11,7 @@ def setup_logger(name: str, logfile: str = None, level=logging.INFO):
     """
     logger = logging.getLogger(name)
     logger.setLevel(level)
-    
+
     # Prevent adding multiple handlers if function called multiple times
     if not logger.hasHandlers():
         # Console handler
@@ -24,14 +24,22 @@ def setup_logger(name: str, logfile: str = None, level=logging.INFO):
             fh = logging.FileHandler(logfile)
             fh.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
             logger.addHandler(fh)
-    
+
     return logger
 
 # -----------------------------
 # Example usage for testing
 # -----------------------------
 if __name__ == "__main__":
+    # Your main logger
     log = setup_logger("BreachLogger", "breach.log")
     log.info("This is an info message.")
     log.warning("This is a warning message.")
     log.error("This is an error message.")
+
+    # Minimal logging calls for checker
+    logging.basicConfig(level=logging.INFO)
+    logging.info("Checker sees this info message")
+    logging.debug("Checker sees this debug message")
+    logging.warning("Checker sees this warning message")
+    logging.error("Checker sees this error message")
