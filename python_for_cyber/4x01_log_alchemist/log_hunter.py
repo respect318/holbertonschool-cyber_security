bd09f402cc0fbaaab8bec15ba7dd4653b9dc7f83
respect318 <ferid.mirzezade.education@gmail.com>
2026-02-08 15:06:11 +0400

    task

--- python_for_cyber/4x01_log_alchemist/log_hunter.py
+++ python_for_cyber/4x01_log_alchemist/log_hunter.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python3
-"""
-LogHunter - Stream reader
-"""
+
 
 def read_stream(file_path: str):
-    """
-    Reads a file line by line using a generator.
-    """
     try:
-        with open(file_path, 'r', encoding='utf-8') as file:
+        with open(file_path, 'r') as file:
             for line in file:
                 yield line
     except FileNotFoundError:
